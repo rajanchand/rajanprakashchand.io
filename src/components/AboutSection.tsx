d5394e87eaf3fff5f65d7b9b394fd965112ac635
Rajan Prakash Chand <rajanprakash.chand08@gmail.com>
2025-04-05 11:53:08 +0100

    Update AboutSection.tsx

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -11,7 +11,7 @@ export default function AboutSection() {
           <div className="lg:w-1/3 animate-slide-in-left">
             <div className="rounded-lg overflow-hidden">
               <img
-                src="/placeholder.svg"
+                src="/rajanchand.JPG"
                 alt="Rajan Prakash Chand"
                 className="w-full object-cover"
               />
